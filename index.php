518ffa2ef8f026320c8dc1a1bc35325cd9145786
2045338 <dagliokan@outlook.com>
2019-11-19 13:13:07 +0100

    Add files via upload
    opdracht 5

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>php opdrachten</title>
+</head>
+<body>
+    <form action="db.php" method="post">
+        leefgebied : <input type="text" name="soort">
+
+        <input type="submit" value="insert">
+    </form>
+
+</body>
+</html>
